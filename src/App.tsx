35be6fef4534f534948fbe513189fc866b2b8c21
Alisson <alissoneatena1@gmail.com>
2026-03-30 16:06:57 -0300

    feat: configure Container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { TimerIcon } from "lucide-react"
-import { Heading } from "./components/Heading"
+import { Container } from "./components/Container"
 import "./styles/global.css"
 
 export const App = () => {
@@ -8,16 +7,29 @@ export const App = () => {
 
     return (
         <>
-            <Heading >
-                Welcome to the Pomodoro Timer!
-                <button>
+            <Container>
+                <section>
 
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>
-                This is a simple Pomodoro timer built with React. It helps you manage your time effectively by breaking work into intervals, traditionally 25 minutes in length, separated by short breaks. The timer will notify you when it's time to take a break or get back to work. Stay focused and boost your productivity with this handy tool!
-            </p>
+                    LOGO
+                </section>
+            </Container>
+            <Container>
+                <section>
+                    MENU
+                </section>
+            </Container>
+            <Container>
+                <section>
+                    <section>
+                        FORM
+                    </section>
+                </section>
+            </Container>
+            <Container>
+                <section>
+                    FOOTER
+                </section>
+            </Container>
         </>
     )
 }
